45c620da98f97dcf2965a77845f2a9e9a35d5b9b
sanjsanj <sanjsanj@hotmail.com>
2015-04-13 16:12:15 +0100

    Test temp can be decreased

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,18 +1,25 @@
 describe('thermostat', function() {
-  
+
   var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   describe('A new thermostat has', function() {
 
     it('a default temp of 20', function() {
-      thermostat = new Thermostat();
       expect(thermostat.temp()).toBe(20);
     });
 
     it('a button to increase temp by one', function(){
-      thermostat = new Thermostat();
       thermostat.increase();
       expect(thermostat.temp()).toBe(21);
+    });
+
+    it('a button to decrease temp by one', function(){
+      thermostat.decrease();
+      expect(thermostat.temp()).toBe(19);
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,12 @@
 var Thermostat = function () {};
 
+var Temp = 20;
+
 Thermostat.prototype.temp = function(){
-  return(20)
+  return(Temp);
+};
+
+Thermostat.prototype.increase = function(){
+  Temp += 1;
+  return(Temp);
 };
